b704c6b73089092da9fd00171e14dcb79f30f31e
abdulrahmancodes <ar5438376@gmail.com>
2023-04-30 20:07:05 +0530

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import Settings from "./pages/Settings";
+import SignIn from "./pages/SignIn.jsx";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 
 import "./assets/scss/main.scss";
-import SignIn from "./pages/SignIn.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Homepage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <PrivateRoute>
+              <Settings />
+            </PrivateRoute>
+          }
+        />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
