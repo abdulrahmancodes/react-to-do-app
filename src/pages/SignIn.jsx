a4f91fd20d268d2a66bdd750549a02e54ce819f1
abdulrahmancodes <ar5438376@gmail.com>
2023-04-30 20:06:26 +0530

    update SignIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import ReactFacebookLogin from "react-facebook-login";
 
 const SignIn = () => {
-  const responseFacebook = (response) => {
-    console.log({ response });
-  };
+  const navigate = useNavigate();
+
+  const responseFacebook = useCallback((response) => {
+    localStorage.setItem("userDetails", JSON.stringify(response));
+  }, []);
+
+  useEffect(() => {
+    const user = localStorage.getItem("userDetails");
+
+    if (user) {
+      navigate("/");
+    }
+  }, []);
 
   return (
     <div className="sign-in-container">
       <ReactFacebookLogin
         appId="628144709171934"
-        // autoLoad={true}
+        autoLoad={true}
         fields="name,email,picture"
         cssClass="facebook-login"
         callback={responseFacebook}
