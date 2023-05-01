09c3318810b2b6128a38b1f32bc83192b9cf1d32
abdulrahmancodes <ar5438376@gmail.com>
2023-05-01 10:46:46 +0530

    update Settings component

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import ToggleSwitch from "../components/ToggleSwitch";
+import { toggleHideCompletedToDos } from "../store/reducers/toDoSlice";
 
 const Settings = () => {
+  const dispatch = useDispatch();
+  const { hideCompletedToDos } = useSelector((state) => state.toDo);
+
   return (
     <div className="settings">
       <h3 className="settings__heading">Settings</h3>
 
       <div className="settings__item">
         <p>Hide Complete TODOs</p>
-        <ToggleSwitch />
+        <ToggleSwitch
+          checked={hideCompletedToDos}
+          onChange={() => dispatch(toggleHideCompletedToDos())}
+        />
       </div>
     </div>
   );
