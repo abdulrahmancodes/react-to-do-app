c241144791d4027098d8bf2397cb7b658b777acc
abdulrahmancodes <ar5438376@gmail.com>
2023-05-01 10:48:44 +0530

    update Navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useCallback } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import { AiOutlineSetting } from "react-icons/ai";
+import { GrLogout } from "react-icons/gr";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const user = localStorage.getItem("userDetails");
+
+  const logout = useCallback(() => {
+    localStorage.clear();
+    navigate("/sign-in");
+  }, []);
+
   return (
     <nav className="navbar">
       <Link to="/">
         <h3 className="navbar__logo">To Do App</h3>
       </Link>
-      <Link to="/settings" className="navbar__settings-link">
-        <AiOutlineSetting className="navbar__settings-icon" />
-      </Link>
+
+      {user && (
+        <div className="navbar__links">
+          <Link to="/settings" className="navbar__settings-link">
+            <AiOutlineSetting className="icon" />
+          </Link>
+          <GrLogout className="icon navbar__logout-link" onClick={logout} />
+        </div>
+      )}
     </nav>
   );
 };
