338ce2144a59ad6ce8cdfc7937eb7b636111fffb
abdulrahmancodes <ar5438376@gmail.com>
2023-05-01 10:48:17 +0530

    pass key prop to TaskDetails and TaskEditor components

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -11,13 +11,20 @@ const Task = ({ toDoItem }) => {
   }, [isEditing]);
 
   if (isEditing) {
-    return <TaskEditor toggleIsEdit={toggleIsEdit} toDoItem={toDoItem} />;
+    return (
+      <TaskEditor
+        toggleIsEdit={toggleIsEdit}
+        toDoItem={toDoItem}
+        key={toDoItem.id}
+      />
+    );
   } else {
     return (
       <TaskDetails
         toggleIsEdit={toggleIsEdit}
         toDoItem={toDoItem}
         isEditing={isEditing}
+        key={toDoItem.id}
       />
     );
   }
