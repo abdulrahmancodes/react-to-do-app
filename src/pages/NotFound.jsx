c2ada073eb64f7833640aac37c946e4e6aea4546
abdulrahmancodes <ar5438376@gmail.com>
2023-05-01 10:47:15 +0530

    create NotFound page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TbError404 } from "react-icons/tb";
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <TbError404 className="not-found-icon" />
+    </div>
+  );
+};
+
+export default NotFound;
