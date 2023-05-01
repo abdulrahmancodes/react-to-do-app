aa6bc66d6375c09634204e9e6b4df6b6a537b4f0
abdulrahmancodes <ar5438376@gmail.com>
2023-05-01 10:47:45 +0530

    render EmptyState component when there's no tasks

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,37 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
+import EmptyState from "./EmptyState";
 import Task from "./Task";
 
 const TasksList = () => {
-  const { todoList } = useSelector((state) => state.toDo);
+  const { todoList, hideCompletedToDos } = useSelector((state) => state.toDo);
 
   const [filteredToDos, setFilteredToDos] = useState(todoList);
   const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-    const filteredToDosList = todoList.filter(({ task }) =>
-      task.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
-    );
+    const filteredToDosList = todoList.filter(({ task, isCompleted }) => {
+      const doesContainSearchText = task
+        .toLocaleLowerCase()
+        .includes(searchText.toLocaleLowerCase());
+
+      if (!hideCompletedToDos) {
+        return doesContainSearchText;
+      }
+
+      return doesContainSearchText && !isCompleted;
+    });
     setFilteredToDos(filteredToDosList);
-  }, [searchText, todoList]);
+  }, [searchText, todoList, hideCompletedToDos]);
 
   return (
     <div className="tasks-list">
       <h3 className="tasks-list__heading">TODO List</h3>
-      <div className="search-bar-container">
-        <input
-          type="text"
-          placeholder="Search"
-          className="search-input"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-      </div>
+      {!!todoList.length && (
+        <div className="search-bar-container">
+          <input
+            type="text"
+            placeholder="Search"
+            className="search-input"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
+      )}
+      {filteredToDos.length ? (
+        <ToDoList filteredToDos={filteredToDos} />
+      ) : (
+        <EmptyState />
+      )}
+    </div>
+  );
+};
+
+const ToDoList = ({ filteredToDos }) => {
+  return (
+    <>
       {filteredToDos.map((todoItem, i) => (
         <Task key={i} toDoItem={todoItem} />
       ))}
-    </div>
+    </>
   );
 };
 
